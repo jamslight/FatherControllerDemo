958afb232c5f57722c9a43b3ffe414ecd8a6bcee
aqiang <aqiang@aqiangdeMacBook-Pro.local>
2018-07-24 16:04:59 +0800

    创建fatherControlelrdemo

---
+++ FatherControllerDemo/FatherControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FatherControllerDemo
+//
+//  Created by aqiang on 2018/7/24.
+//  Copyright © 2018年 aqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
